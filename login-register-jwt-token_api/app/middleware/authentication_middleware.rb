77be65da47875cef87479c6ca9fb5ef486f257ff
pkindalov <panayotkindalov@gmail.com>
2024-07-20 13:17:29 +0300

    Added some exercises

---
+++ login-register-jwt-token_api/app/middleware/authentication_middleware.rb
@@ -0,0 +1,58 @@
+# app/middleware/auth_middleware.rb
+require 'jwt'
+
+class AuthenticationMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+
+    if excluded_path?(request.path)
+      @app.call(env)
+    else
+      token = extract_token(request)
+
+      if valid_token?(token)
+        @app.call(env)
+      else
+        [401, { 'Content-Type' => 'application/json' }, [{ error: 'Unauthorized' }.to_json]]
+      end
+    end
+  end
+
+  private
+
+  def excluded_path?(path)
+    # Проверка на пътища за логване и регистрация
+    excluded_paths = [
+      '/api/v1/login',
+      '/api/v1/register'
+    ]
+
+    # Преобразува пътя в относителен формат
+    path = path.sub(/^\/api\/v1\//, '/')
+
+    excluded_paths.any? { |excluded_path| path.start_with?(excluded_path) }
+  end
+
+  def extract_token(request)
+    authorization_header = request.get_header('HTTP_AUTHORIZATION')
+    token = authorization_header&.split(' ')&.last
+    Rails.logger.info "Extracted Token: #{token}"  # За отстраняване на проблеми
+    token
+  end
+
+  def valid_token?(token)
+    return false unless token
+
+    begin
+      decoded_token = JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')
+      Rails.logger.info "Decoded Token: #{decoded_token.inspect}"  # За отстраняване на проблеми
+      decoded_token.present?
+    rescue JWT::DecodeError
+      false
+    end
+  end
+end
---
+++ login-register-jwt-token_api/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      post 'register', to: 'auth#register'
+      post 'login', to: 'auth#login'
+    end
+  end
+end
